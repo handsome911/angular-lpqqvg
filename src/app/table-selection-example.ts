3356beb4de373a004d0fab610ef17e0052a0403c
handsome911 <sergey@uasoft.com.ua>
2020-12-15 16:51:53 +0200

    2 files modified

--- src/app/table-selection-example.ts
+++ src/app/table-selection-example.ts
@@ -3,23 +3,23 @@ import {Component} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 
 export interface PeriodicElement {
-  name: string;
   position: number;
-  weight: number;
+  name: string;
+  weight: string;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 0, name: 'Hydrogen (201)',  weight: '+380991005201', symbol: 'main (+380991005001)'},
+  {position: 1, name: 'Helium (202)',    weight: '+380991005202', symbol: 'main (+380991005001)'},
+  {position: 2, name: 'Lithium (305)',   weight: '+380991005305', symbol: 'sales (+380991005002)'},
+  {position: 3, name: 'Beryllium (306)', weight: '+380991005306', symbol: 'sales (+380991005002)'},
+  {position: 4, name: 'Boron',           weight: '',              symbol: ''},
+  {position: 5, name: 'Carbon',          weight: '+380991005306', symbol: 'ERROR. SELECT BILLING'},
+  {position: 6, name: 'Fluorine',        weight: '',              symbol: ''},
+  {position: 7, name: 'Nitrogen (501)',  weight: '+380991007777',  symbol: 'logistics (+380999999999)'},
+  {position: 8, name: 'Oxygen (502)',    weight: '+380991007778',  symbol: 'logistics (+380999999999)'},
+  {position: 9, name: 'Neon',            weight: '',              symbol: ''},
 ];
 
 /**
@@ -31,7 +31,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: 'table-selection-example.html',
 })
 export class TableSelectionExample {
-  displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['select', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
 
